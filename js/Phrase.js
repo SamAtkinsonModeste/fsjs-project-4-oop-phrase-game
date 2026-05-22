3ff3757c62e226e04400fe17c36d51697e0a4cf3
Sam Atkinson Modeste <sammodeste@yahoo.com>
2026-05-22 11:54:40 +0100

    feat: (phrase): add addPhraseToDisplay method
    - create phrase letter elements - display letters and spaces on gameboard - add helper method for DOM element creation

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,4 +7,48 @@ class Phrase {
     this.question = question;
     this.phrase = phrase.toLowerCase();
   }
+
+  createLetterElements(nameElement, property, value) {
+    nameElement = document.createElement(nameElement);
+    nameElement[property] = value;
+    return nameElement;
+  }
+
+  addPhraseToDisplay() {
+    let questionContainer = document.querySelector(".header");
+    questionContainer.textContent = this.question;
+    const ulPhrase = document.querySelector("#phrase ul");
+    const letters = this.phrase.split("");
+    letters.forEach((letter) => {
+      if (letter !== " ") {
+        const li = this.createLetterElements("li", "className", "letter");
+        const spanBox = this.createLetterElements("span", "className", "box");
+        const spanFront = this.createLetterElements(
+          "span",
+          "className",
+          "front",
+        );
+        const spanLetter = this.createLetterElements(
+          "span",
+          "className",
+          "letter",
+        );
+        spanBox.appendChild(spanFront);
+        spanBox.appendChild(spanLetter);
+        li.appendChild(spanBox);
+        spanLetter.textContent = letter;
+        ulPhrase.appendChild(li);
+
+        console.log(li);
+      } else {
+        const liSpace = this.createLetterElements("li", "className", "space");
+        ulPhrase.appendChild(liSpace);
+
+        console.log(liSpace);
+      }
+    });
+    console.log(questionContainer);
+    console.log(letters);
+    console.log(ulPhrase);
+  }
 }
