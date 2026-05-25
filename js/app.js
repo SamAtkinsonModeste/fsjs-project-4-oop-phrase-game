42be98ca78e117d7fbdc7aaa29a67ccce5e17f76
Sam Atkinson Modeste <sammodeste@yahoo.com>
2026-05-25 13:04:23 +0100

    feat(app) evt listener on start btn triggers startGame method

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+let game;
+const startBtn = document.querySelector("#btn__reset");
 
-const game = new Game();
-
-game.phrases.forEach((phrase, index) => {
-  console.log(
-    `Phrase ${index} - question: ${phrase.question} | answer: ${phrase.phrase} `,
-  );
+startBtn.addEventListener("click", () => {
+  game = new Game();
+  game.startGame();
 });
-
-console.log(game.getRandomPhrase());
-console.log(game.getRandomPhrase());
-console.log(game.getRandomPhrase());
-console.log(game.getRandomPhrase());
-console.log(game.getRandomPhrase());
-console.log(game.getRandomPhrase());
