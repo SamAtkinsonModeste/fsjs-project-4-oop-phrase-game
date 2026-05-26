330c2a87e530f515afb3131ba94827a9e329037c
Sam Atkinson Modeste <sammodeste@yahoo.com>
2026-05-26 14:48:00 +0100

    feat: evt listener on qwerty buttons (keyboard)

--- js/app.js
+++ js/app.js
@@ -3,8 +3,17 @@
  * app.js */
 let game;
 const startBtn = document.querySelector("#btn__reset");
+const qwertySection = document.querySelector("#qwerty");
 
 startBtn.addEventListener("click", () => {
   game = new Game();
   game.startGame();
+  console.log(game.activePhrase);
+});
+
+qwertySection.addEventListener("click", (evt) => {
+  if (evt.target.tagName === "BUTTON") {
+    const keyboardBtn = evt.target;
+    game.handleInteraction(keyboardBtn);
+  }
 });
